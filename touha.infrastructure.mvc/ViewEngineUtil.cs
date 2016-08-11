52a9e743dc92030cde333c5bd50910efc75d52ef
javaDevp <1129597198@qq.com>
2016-08-11 10:39:40 +0800

    新增mvc关于视图引擎设置，webapi过滤请求格式内容

---
+++ touha.infrastructure.mvc/ViewEngineUtil.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace touha.infrastructure.mvc
+{
+    public class ViewEngineUtil
+    {
+        /// <summary>
+        /// 将视图引擎设置为只支持Razor，格式仅支持cshtml
+        /// </summary>
+        public static void SetViewEngineRazorWithCshtml()
+        {
+            //清除WebFormViewEngine
+            ViewEngines.Engines.RemoveAt(0);
+
+            RazorViewEngine razor = ViewEngines.Engines[0] as RazorViewEngine;
+            razor.AreaViewLocationFormats = new string[] 
+            {
+                "~/Areas/{2}/Views/{1}/{0}.cshtml",
+                //"~/Areas/{2}/Views/{1}/{0}.vbhtml",
+                "~/Areas/{2}/Views/Shared/{0}.cshtml"
+                //"~/Areas/{2}/Views/Shared/{0}.vbhtml"
+            };
+            razor.AreaMasterLocationFormats = new string[]
+            {
+                "~/Areas/{2}/Views/{1}/{0}.cshtml",
+               // "~/Areas/{2}/Views/{1}/{0}.vbhtml",
+                "~/Areas/{2}/Views/Shared/{0}.cshtml"
+                //"~/Areas/{2}/Views/Shared/{0}.vbhtml"
+            };
+            razor.AreaPartialViewLocationFormats = new string[]
+            {
+                "~/Areas/{2}/Views/{1}/{0}.cshtml",
+                //"~/Areas/{2}/Views/{1}/{0}.vbhtml",
+                "~/Areas/{2}/Views/Shared/{0}.cshtml"
+                //"~/Areas/{2}/Views/Shared/{0}.vbhtml"
+            };
+            razor.ViewLocationFormats = new[]
+            {
+                "~/Views/{1}/{0}.cshtml",
+                //"~/Views/{1}/{0}.vbhtml",
+                "~/Views/Shared/{0}.cshtml"
+                //"~/Views/Shared/{0}.vbhtml"
+            };
+            razor.MasterLocationFormats = new[]
+            {
+                "~/Views/{1}/{0}.cshtml",
+                //"~/Views/{1}/{0}.vbhtml",
+                "~/Views/Shared/{0}.cshtml"
+                //"~/Views/Shared/{0}.vbhtml"
+            };
+            razor.PartialViewLocationFormats = new[]
+            {
+                "~/Views/{1}/{0}.cshtml",
+                //"~/Views/{1}/{0}.vbhtml",
+                "~/Views/Shared/{0}.cshtml"
+                //"~/Views/Shared/{0}.vbhtml"
+            };
+            razor.FileExtensions = new[]
+            {
+                "cshtml"
+                //"vbhtml"
+            };
+        }
+    }
+}
---
+++ touha.infrastructure.webapi/WebApiFormatter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace touha.infrastructure.webapi
+{
+    public class WebApiFormatter
+    {
+        /// <summary>
+        /// 移除WebApi默认的xml格式
+        /// 用法：WebApiFormatter.RemoveXmlFormat(GlobalConfiguration.Configuration);
+        /// </summary>
+        /// <param name="config"></param>
+        public static void RemoveXmlFormat(HttpConfiguration config)
+        {
+            #region 方式1（推荐）：可去除自定义Formatter中包含的xml格式
+            var matches = config.Formatters.
+                Where(f => f.SupportedMediaTypes.
+                Where(m => m.MediaType == "application/xml" || m.MediaType == "text/xml").
+                Any()).ToList();
+            foreach(var match in matches)
+            {
+                config.Formatters.Remove(match);
+            }
+            #endregion
+
+            #region 方式2（不推荐）
+            config.Formatters.Remove(config.Formatters.XmlFormatter);
+            #endregion
+        }
+    }
+}
